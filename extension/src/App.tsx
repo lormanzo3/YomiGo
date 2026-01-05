90f5c1ecc0b39fd68157a012a98c373a6c3df33d
Lorenzo Panneman <unframed3@gmail.com>
2026-01-05 11:14:17 +0900

    Added image uploader

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,8 +1,26 @@
+import React, { useState } from 'react'
+
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0]
+    if (file) {
+      setSelectedFile(file)
+    }
+  }
+
   return (
     <div>
       <h1>YomiGo</h1>
-      <p>Japanese manga reader</p>
+
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+      />
+
+      {selectedFile && <p>Selected: {selectedFile.name}</p>}
     </div>
   )
 }
